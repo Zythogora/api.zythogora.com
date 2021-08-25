f8e50f42d92f1ace177cde448fb010d0e95c7395
Carl-Adrien Mercey <camercey@gmail.com>
2021-08-25 19:06:37 +0200

    update GET /styles behavior

--- routers/styles.py
+++ routers/styles.py
@@ -20,23 +20,15 @@ async def get_style(style_id: int, api_key : APIKey = Depends(get_api_key)):
     if not query_styles:
         raise HTTPException(status_code=404, detail="The style you requested does not exist.")
 
-    if cursor.rowcount == 1:
-        return {
-            "id": query_styles[0][0],
-            "name": query_styles[0][1],
-            "substyle_id": query_styles[0][2],
-            "substyle_name": query_styles[0][3]
-        }
-    else:
-        styles = []
-        for row in query_styles:
-            styles.append({
-                "id": row[0],
-                "name": row[1],
-                "substyle_id": row[2],
-                "substyle_name": row[3]
-            })
-        return styles
+    styles = []
+    for row in query_styles:
+        styles.append({
+            "id": row[0],
+            "name": row[1],
+            "substyle_id": row[2],
+            "substyle_name": row[3]
+        })
+    return styles
 
 @router.get("/styles/{style_id}/{substyle_id}/", tags=["styles"])
 async def get_style(style_id: int, substyle_id: int, api_key : APIKey = Depends(get_api_key)):
