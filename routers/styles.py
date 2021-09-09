8a4020af0094357847049267514820fe09e6fe97
Carl-Adrien Mercey <camercey@gmail.com>
2021-09-09 11:56:09 +0200

    add POST /styles

--- routers/styles.py
+++ routers/styles.py
@@ -1,8 +1,85 @@
-from config import cursor, get_api_key
+from config import connection, cursor, get_api_key
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security.api_key import APIKey
+from pydantic import BaseModel
 
 router = APIRouter()
+
+
+
+class Style(BaseModel):
+    name: str
+    substyle_name: str = None
+
+@router.post("/styles", tags=["styles"])
+async def add_style(style: Style, api_key : APIKey = Depends(get_api_key)):
+    style_id = None
+    substyle_id = None
+
+    cursor.execute("SELECT id FROM Styles WHERE name = %s", (style.name, ))
+    query_styles = cursor.fetchone()
+
+    if query_styles:
+        style_id = query_styles[0]
+
+        if style.substyle_name:
+            cursor.execute(
+                "SELECT Styles.id AS id FROM Styles " +
+                "LEFT JOIN SubStyles ON Styles.id = SubStyles.style " +
+                "WHERE Styles.name = %s AND SubStyles.name = %s"
+            , (style.name, style.substyle_name))
+            query_substyles = cursor.fetchone()
+
+            if query_substyles:
+                raise HTTPException(status_code=409, detail="This style already exists.")
+
+            else:
+                cursor.execute(
+                    "INSERT INTO SubStyles " +
+                    "(name, style, added_by) " +
+                    "VALUES (%s, %s, %s)"
+                , (
+                    style.substyle_name,
+                    style_id,
+                    api_key["user"]
+                ))
+                connection.commit()
+                substyle_id = cursor.lastrowid
+
+        else:
+            raise HTTPException(status_code=409, detail="This style already exists.")
+
+    else:
+        cursor.execute(
+            "INSERT INTO Styles " +
+            "(name, added_by) " +
+            "VALUES (%s, %s)"
+        , (
+            style.name,
+            api_key["user"]
+        ))
+        connection.commit()
+        style_id = cursor.lastrowid
+
+        if style.substyle_name:
+            cursor.execute(
+                "INSERT INTO SubStyles " +
+                "(name, style, added_by) " +
+                "VALUES (%s, %s, %s)"
+            , (
+                style.substyle_name,
+                style_id,
+                api_key["user"]
+            ))
+            connection.commit()
+            substyle_id = cursor.lastrowid
+
+    return {
+        "id": style_id,
+        "name": style.name,
+        "substyle_id": substyle_id,
+        "substyle_name": style.substyle_name
+    }
 
 
 
@@ -29,6 +106,8 @@ async def get_style(style_id: int, api_key : APIKey = Depends(get_api_key)):
             "substyle_name": row[3]
         })
     return styles
+
+
 
 @router.get("/styles/{style_id}/{substyle_id}/", tags=["styles"])
 async def get_style(style_id: int, substyle_id: int, api_key : APIKey = Depends(get_api_key)):
