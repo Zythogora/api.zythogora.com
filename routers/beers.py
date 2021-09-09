a047b8a1e55ec6539b99f0787f2280342c4179e1
Carl-Adrien Mercey <camercey@gmail.com>
2021-09-09 11:57:50 +0200

    add POST /beers

--- routers/beers.py
+++ routers/beers.py
@@ -1,8 +1,69 @@
-from config import cursor, get_api_key
+from config import connection, cursor, get_api_key
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security.api_key import APIKey
+from pydantic import BaseModel
 
 router = APIRouter()
+
+
+
+class Beer(BaseModel):
+    name: str
+    brewery: int
+    style: int
+    substyle: int = None
+    abv: float
+    ibu: int
+    color: int
+
+@router.post("/beers", tags=["beers"])
+async def add_beer(beer: Beer, api_key : APIKey = Depends(get_api_key)):
+    cursor.execute("SELECT id FROM Beers WHERE name=%s AND brewery=%s", (beer.name, beer.brewery ))
+    query_beers = cursor.fetchone()
+    if query_beers:
+        raise HTTPException(status_code=409, detail="This beer already exists (" + str(query_beers[0]) + ").")
+
+    if beer.substyle:
+        cursor.execute(
+            "INSERT INTO Beers " +
+            "(name, brewery, style, substyle, abv, ibu, color, added_by)" +
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        , (
+            beer.name,
+            beer.brewery,
+            beer.style,
+            beer.substyle,
+            beer.abv,
+            beer.ibu,
+            beer.color,
+            api_key["user"]
+        ))
+    else:
+        cursor.execute(
+            "INSERT INTO Beers " +
+            "(name, brewery, style, abv, ibu, color, added_by)" +
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        , (
+            beer.name,
+            beer.brewery,
+            beer.style,
+            beer.abv,
+            beer.ibu,
+            beer.color,
+            api_key["user"]
+        ))
+    connection.commit()
+
+    return {
+        "id": cursor.lastrowid,
+        "name": beer.name,
+        "beer": beer.brewery,
+        "style": beer.style,
+        "substyle": beer.substyle,
+        "abv": beer.abv,
+        "ibu": beer.ibu,
+        "color": beer.color
+    }
 
 
 
@@ -28,3 +89,5 @@ async def get_beer(beer_id: int, api_key : APIKey = Depends(get_api_key)):
         "ibu": query_beers[6],
         "color": query_beers[7]
     }
+
+
