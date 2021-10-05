06e8fb25c77a838836db7c73620efe2b310162eb
Carl-Adrien Mercey <camercey@gmail.com>
2021-10-05 21:31:39 +0200

    add POST /users/login and POST /users/register

--- routers/users.py
+++ routers/users.py
@@ -1,6 +1,11 @@
-from config import cursor, get_api_key, search
+from argon2 import PasswordHasher
+from config import connection, cursor, get_api_key, search
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security.api_key import APIKey
+import jwt
+import os
+from pydantic import BaseModel
+import time
 
 import routers.ratings as r_ratings
 
@@ -8,14 +13,94 @@ router = APIRouter()
 
 
 
-@router.get("/users/{user_id}", tags=["users"])
-async def get_user(user_id: int, api_key : APIKey = Depends(get_api_key)):
-    cursor.execute(
-        "SELECT Users.id, Users.username, Users.nationality, COUNT(Ratings.id) AS ratings " +
-        "FROM Ratings " +
-        "JOIN Users ON Ratings.user = Users.id " +
-        "WHERE Users.id = %s"
-    , (user_id,))
+class Login(BaseModel):
+    username: str
+    password: str
+
+@router.post("/users/login", tags=["users"])
+async def login(login: Login, api_key : APIKey = Depends(get_api_key)):
+    cursor.execute("""
+        SELECT uuid, username, password_hash
+        FROM Users
+        WHERE username=%s
+    """, (login.username,))
+    query_users = cursor.fetchone()
+
+    if not query_users:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    try:
+        PasswordHasher().verify(query_users[2], login.password)
+    except:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    now = int(time.time())
+    return jwt.encode(
+        {
+            "client_id": query_users[0],
+            "nickname": query_users[1],
+            "iat": now,
+            "exp": now + 60 * 60 * 24 * 7 * 6,
+        }, os.environ["zythogora_jwt_secret"], algorithm="HS512")
+
+
+
+class Register(BaseModel):
+    firstname: str
+    lastname: str
+    email: str
+    username: str
+    password: str
+    nationality: int
+
+@router.post("/users/register", tags=["users"])
+async def register(register: Register, api_key : APIKey = Depends(get_api_key)):
+    cursor.execute("""
+        SELECT id
+        FROM Users
+        WHERE username=%s OR email=%s
+    """, (register.username, register.email))
+    query_users = cursor.fetchone()
+
+    if query_users:
+        raise HTTPException(status_code=409, detail="This username / email is already in use")
+
+    cursor.execute("""
+        INSERT INTO Users
+        (firstname, lastname, username, email, password_hash, nationality)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (
+        register.firstname,
+        register.lastname,
+        register.username,
+        register.email,
+        PasswordHasher().hash(register.password),
+        register.nationality
+    ))
+    connection.commit()
+
+    cursor.execute("SELECT uuid FROM Users WHERE id=%s", (cursor.lastrowid, ))
+    query_users = cursor.fetchone()
+
+    now = int(time.time())
+    return jwt.encode(
+        {
+            "client_id": query_users[0],
+            "nickname": register.username,
+            "iat": now,
+            "exp": now + 60 * 60 * 3,
+        }, os.environ["zythogora_jwt_secret"], algorithm="HS512")
+
+
+
+@router.get("/users/{user_uuid}", tags=["users"])
+async def get_user(user_uuid: str, api_key : APIKey = Depends(get_api_key)):
+    cursor.execute("""
+        SELECT Users.uuid, Users.username, Users.nationality, COUNT(Ratings.id) AS ratings
+        FROM Ratings
+        JOIN Users ON Ratings.user=Users.uuid
+        WHERE Users.uuid=%s
+    """, (user_uuid,))
     query_users = cursor.fetchone()
 
     if not query_users:
@@ -30,13 +115,19 @@ async def get_user(user_id: int, api_key : APIKey = Depends(get_api_key)):
 
 
 
-@router.get("/users/{user_id}/ratings", tags=["users"])
-async def get_user_ratings(user_id: int, api_key : APIKey = Depends(get_api_key)):
+@router.get("/users/{user_uuid}/ratings", tags=["users"])
+async def get_user_ratings(user_uuid: str, api_key : APIKey = Depends(get_api_key)):
+    cursor.execute("SELECT uuid FROM Users WHERE uuid=%s", (user_uuid,))
+    query_users = cursor.fetchone()
+
+    if not query_users:
+        raise HTTPException(status_code=404, detail="The user you requested does not exist.")
+
     cursor.execute("""
         SELECT id FROM Ratings
-        WHERE user = %s
+        WHERE user=%s
         ORDER BY date DESC
-    """, (user_id,))
+    """, (user_uuid,))
     query_ratings = cursor.fetchall()
 
     res = [ ]
@@ -50,7 +141,7 @@ async def get_user_ratings(user_id: int, api_key : APIKey = Depends(get_api_key)
 
 @router.get("/users/search/{username}", tags=["users"])
 async def search_user(username: str, count: int = 10, api_key: APIKey = Depends(get_api_key)):
-    cursor.execute("SELECT id, username FROM Users")
+    cursor.execute("SELECT uuid, username FROM Users")
     query_users = cursor.fetchall()
 
     user_ids = await search(username, query_users, count)
