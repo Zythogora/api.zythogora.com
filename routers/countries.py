53f434ec33d58d95b8e1c04e65611600d5253041
Kittease <88965990+Kittease@users.noreply.github.com>
2022-03-14 01:07:59 +0100

    add GET /countries

--- routers/countries.py
+++ routers/countries.py
@@ -27,6 +27,28 @@ async def get_country(country_id: int, api_key : APIKey = Depends(get_api_key)):
 
 
 
+@router.get("/countries", tags=["countries"])
+async def get_countries(api_key : APIKey = Depends(get_api_key)):
+    with connection.cursor(prepared=True) as cursor:
+        cursor.execute("""
+            SELECT id
+            FROM Countries
+            ORDER BY name
+        """)
+        query_countries = cursor.fetchall()
+
+        if not query_countries:
+            raise HTTPException(status_code=404, detail="The country you requested does not exist.")
+
+        res = [ ]
+        for el in query_countries:
+            data = await get_country(el[0], api_key)
+            res.append(data)
+
+        return res
+
+
+
 @router.get("/countries/search/{country_name}", tags=["countries"])
 async def search_country(country_name: str, count: int = 10, page: int = 1, api_key: APIKey = Depends(get_api_key)):
     with connection.cursor(prepared=True) as cursor:
