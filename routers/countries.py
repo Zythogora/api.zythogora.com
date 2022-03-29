3b5e07e06606d85d0743815af399b34c77524cb6
Kittease <camercey@gmail.com>
2022-03-30 00:02:54 +0200

    remove GET /countries and /countries/{id} tmp endpoints

--- routers/countries.py
+++ routers/countries.py
@@ -6,12 +6,12 @@ router = APIRouter()
 
 
 
-@router.get("/countries/v2/{country_id}", tags=["countries_v2"])
-async def get_country_v2(country_id: int, api_key : APIKey = Depends(get_api_key)):
+@router.get("/countries/{country_id}", tags=["countries"])
+async def get_country(country_id: int, api_key : APIKey = Depends(get_api_key)):
     with connection.cursor(prepared=True) as cursor:
         cursor.execute("""
             SELECT id, name, code, flag, phone
-            FROM Countries_v2
+            FROM Countries
             WHERE id=%s
         """, (country_id,))
         query_countries = cursor.fetchone()
@@ -25,46 +25,6 @@ async def get_country_v2(country_id: int, api_key : APIKey = Depends(get_api_key
             "code": query_countries[2],
             "flag": query_countries[3],
             "phone": query_countries[4],
-        }
-
-
-
-@router.get("/countries/v2", tags=["countries_v2"])
-async def get_countries_v2(api_key : APIKey = Depends(get_api_key)):
-    with connection.cursor(prepared=True) as cursor:
-        cursor.execute("""
-            SELECT id
-            FROM Countries_v2
-            ORDER BY name
-        """)
-        query_countries = cursor.fetchall()
-
-        res = [ ]
-        for el in query_countries:
-            data = await get_country_v2(el[0], api_key)
-            res.append(data)
-
-        return res
-
-
-
-@router.get("/countries/{country_id}", tags=["countries"])
-async def get_country(country_id: int, api_key : APIKey = Depends(get_api_key)):
-    with connection.cursor(prepared=True) as cursor:
-        cursor.execute("""
-            SELECT id, name, flag
-            FROM Countries
-            WHERE id=%s
-        """, (country_id,))
-        query_countries = cursor.fetchone()
-
-        if not query_countries:
-            raise HTTPException(status_code=404, detail="The country you requested does not exist.")
-
-        return {
-            "id": query_countries[0],
-            "name": query_countries[1],
-            "flag": query_countries[2].strip()
         }
 
 
